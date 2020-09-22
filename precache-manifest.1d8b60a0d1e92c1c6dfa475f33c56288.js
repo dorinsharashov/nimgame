99a2e256af0d535ab9150dbaaa25d30732b60b10
Dorin Sharashov <dorin.sharashov95@gmail.com>
2020-09-22 20:21:23 +0300

    Updates

---
+++ precache-manifest.1d8b60a0d1e92c1c6dfa475f33c56288.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d98cac14372ba2d075288ec512d09ed",
+    "url": "/nimgame/index.html"
+  },
+  {
+    "revision": "55be5c9aeff4345dc6b2",
+    "url": "/nimgame/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3c103935d777e9b6228f",
+    "url": "/nimgame/static/css/main.71ca2367.chunk.css"
+  },
+  {
+    "revision": "55be5c9aeff4345dc6b2",
+    "url": "/nimgame/static/js/2.89e92b00.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/nimgame/static/js/2.89e92b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c103935d777e9b6228f",
+    "url": "/nimgame/static/js/main.e9b0e8d6.chunk.js"
+  },
+  {
+    "revision": "d9bc9b129b690a74f97d",
+    "url": "/nimgame/static/js/runtime-main.fc7b62bc.js"
+  }
+]);
